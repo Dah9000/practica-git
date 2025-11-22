240e109e81f42be13f779189551d48be5bcecd57
Daniel Marquez Garcia <marquezgarcia.daniel@outlook.es>
2025-11-22 12:54:10 +0100

    Se ha añadido el programa en C practica-git.c

---
+++ src/practica-git.c
@@ -0,0 +1,13 @@
+/*
+ * hola.c
+ *
+ *  Created on: 22 nov 2025
+ *      Author: marqu
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Practica de Git con codigo en C\n");
+    return 0;
+}
